1abddbcdaf3d2083f4d9c9aa969a8e5a7efc8649
nishacodes <nishabatra27@gmail.com>
2014-04-27 16:56:36 -0400

    create new app and header

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FedoraDemo::Application.config.secret_token = '5c194aa2d0e01cdad1c7dc1f53004042bc456c0e33b0983308ec2f87f182662cb63036d2fe717ec7b198b821d48f882b3b467137a9a30b9c8209f09b661d91a0'
