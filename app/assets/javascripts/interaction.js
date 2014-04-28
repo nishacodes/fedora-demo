b06fab37491b93da7f7047f3b38297ec942d6d20
nishacodes <nishabatra27@gmail.com>
2014-04-28 01:00:20 -0400

    style sidebar and create detailss section with tabs

--- app/assets/javascripts/interaction.js
+++ app/assets/javascripts/interaction.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+  // Sliding sidebar
   $('#sidebar-button').on('click', function(){
     $(this).toggleClass('hide');
     if ($(this).hasClass('hide')) {
@@ -15,5 +16,30 @@ $(document).ready(function(){
   //   $(".sidebar").toggleClass("sidebar-shift");  
   //   $(".content").toggleClass("content-shift");  
   // })
+
+  // Highlight active menu item
+  $("#sidebar li.item").on('click', function(){
+    // $('#sidebar li.item').removeClass('active');
+    $(this).addClass('active').siblings().removeClass('active');
+  })
+
+  // Tabs
+  $('.tabs li a').on('click', function(e)  {
+    e.preventDefault();
+    var currentAttrValue = $(this).attr('href');
+    $('.tab-content ' + currentAttrValue).show().siblings().hide();  
+    $(this).parent('li').addClass('active').siblings().removeClass('active');
+  });
+
+  // Show / hide details section
+  $('#arrow-button-left').on('click', function(){
+    $('.details').show();
+    $(this).hide();
+  })
+
+  $('#arrow-button-right').on('click', function(){
+    $('.details').hide();
+    $('#arrow-button-left').show();
+  })
 
 })
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,22 +4,22 @@ class PagesController < ApplicationController
     @sidenav = {
       "Section 1" => [
         {"icon-class"=> "fa fa-youtube-play", "name"=> "Video Demo (with attachments)" },
-        {"icon-class"=> "fa fa-youtube-play", "name"=> "PDF Demo" }
+        {"icon-class"=> "fa fa-file-text-o", "name"=> "PDF Demo" }
       ],
       "Section 2" => [
-        {"icon-class"=> "fa fa-youtube-play", "name"=> "Text Demo" },
-        {"icon-class"=> "fa fa-youtube-play", "name"=> "PPT Demo" }
+        {"icon-class"=> "fa fa-file-text-o", "name"=> "Text Demo" },
+        {"icon-class"=> "fa fa-picture-o", "name"=> "PPT Demo" }
       ],
       "Section 3" => [
-        {"icon-class"=> "fa fa-youtube-play", "name"=> "Audio Demo" },
-        {"icon-class"=> "fa fa-youtube-play", "name"=> "Google Doc (Editable)" },
-        {"icon-class"=> "fa fa-youtube-play", "name"=> "Quiz Demo" },
-        {"icon-class"=> "fa fa-youtube-play", "name"=> "Survey Demo" },
-        {"icon-class"=> "fa fa-youtube-play", "name"=> "Google Form Demo" },
+        {"icon-class"=> "fa fa-volume-up", "name"=> "Audio Demo" },
+        {"icon-class"=> "fa fa-file-text-o", "name"=> "Google Doc (Editable)" },
+        {"icon-class"=> "fa fa-file-text-o", "name"=> "Quiz Demo" },
+        {"icon-class"=> "fa fa-table", "name"=> "Survey Demo" },
+        {"icon-class"=> "fa fa-table", "name"=> "Google Form Demo" },
         {"icon-class"=> "fa fa-youtube-play", "name"=> "YouTube Livestream Demo" },
-        {"icon-class"=> "fa fa-youtube-play", "name"=> "Google Groups Demo" },
-        {"icon-class"=> "fa fa-youtube-play", "name"=> "Flipbook Demo" },
-        {"icon-class"=> "fa fa-youtube-play", "name"=> "Articulate Demo" }
+        {"icon-class"=> "fa fa-file-text-o", "name"=> "Google Groups Demo" },
+        {"icon-class"=> "fa fa-file-text-o", "name"=> "Flipbook Demo" },
+        {"icon-class"=> "fa fa-file-text-o", "name"=> "Articulate Demo" }
       ]
     }
   end
